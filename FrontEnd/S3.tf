a986cc3c54f287d4c362619535ad53c81b02ccfc
dcodez23 <71770133+dcodez23@users.noreply.github.com>
2023-06-17 21:47:32 -0400

    S3 Bucket Policy Update

--- FrontEnd/S3.tf
+++ FrontEnd/S3.tf
@@ -47,8 +47,10 @@ resource "aws_s3_bucket_object" "s3_upload" {
 resource "aws_s3_bucket_public_access_block" "danzPublic" {
   bucket = aws_s3_bucket.bucketSiteName.id
 
-  block_public_acls   = false
-  block_public_policy = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "danzBucketPolicy" {

fe7b431d1f7623dac4a2a77360d119870bc4a978
atapazD <daniel.zapata1011@gmail.com>
2023-12-10 18:32:28 -0500

    Removed unneeded resource creation

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -31,16 +31,16 @@ module "s3_bucket" {
 
 }
 
-module "dynamodb" {
-  source     = "../../modules/dynamodb"
-  table_name = var.table_name
+# module "dynamodb" {
+#   source     = "../../modules/dynamodb"
+#   table_name = var.table_name
 
-}
+# }
 
-module "lambda" {
-  source             = "../../modules/lambda"
-  table_name         = var.table_name
-  dynamodb_table_arn = module.dynamodb.dynamodb_table_arn
-  environment        = var.environment
-}
-#updated dev environment!!
+# module "lambda" {
+#   source             = "../../modules/lambda"
+#   table_name         = var.table_name
+#   dynamodb_table_arn = module.dynamodb.dynamodb_table_arn
+#   environment        = var.environment
+# }
+# #updated dev environment!
